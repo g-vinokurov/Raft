c585c27ac04cece8276ffa1ffd14358b7ac72647
g-vinokurov <g.vinokurov@g.nsu.ru>
2025-03-27 04:18:50 +0700

    Fix bugs, add Append Entries Request sending, and some else

--- raft.py
+++ raft.py
@@ -114,7 +114,7 @@ class RaftLog:
     def last(self):
         return self._entries[-1]
     
-    def __getitem__(self, key: int) -> RaftLogEntry:
+    def __getitem__(self, key: int | slice) -> RaftLogEntry:
         return self._entries[key]
     
     def __len__(self):
@@ -163,10 +163,10 @@ class RaftServer:
             self.stop()
     
     def stop(self):
-        self.log('Stopping...')
+        self.echo('Stopping...')
         self._is_running = False
 
-    def log(self, msg):
+    def echo(self, msg):
         if not self._echo:
             return
         print(msg)
@@ -213,53 +213,68 @@ class RaftServer:
                         return await response.json()
                     return None
         except Exception as e:
-            self.log(f'Error sending RPC to {url}: {e}')
+            self.echo(f'Error sending RPC to {url}: {e}')
             return None
     
     async def _reset_election_timer(self):
+        # Reset timer with new random value of timeout
+        # In order to avoid more than one elections for many vote attempts
         self._election_timeout = 500 + random.randint(0, 200)
         self._election_timer = Timer(self._election_timeout, self._start_election)
         await self._election_timer.start()
     
     async def _start_election(self):
+        # If node has become Leader and election timer callback was called,
+        # new election will not be started
         if self._state == RaftServerState.Leader:
             return
         
-        self.log('Start election...')
-
+        self.echo('Start election...')
+        
+        # On conversion to candidate, start election
         if self._state == RaftServerState.Follower:
-            self.log('I was Follower, now I am Candidate')
+            self.echo('I was Follower, now I am Candidate')
             self._state = RaftServerState.Candidate
         
+        # Increment current term
         self._current_term += 1
-        self._voted_for = self._this
-        
+        # Vote for self
+        self._voted_for = self._this 
         votes = 1
+        
+        # Reset election timer
+        await self._reset_election_timer()
+        
+        # Send RequestVote RPCs to all other servers
         votes += await self._request_votes()
-
-        self.log(f'Total Votes: {votes}')
-
+        
+        self.echo(f'Total Votes: {votes}')
+        
+        # If votes received from majority of servers: become leader
         if votes > len(self._others) / 2:
-            self.log('I try to become Leader')
+            self.echo('I try to become Leader')
             await self._become_leader()
         else:
-            self.log('I got too few votes, now I am Follower again')
+            self.echo('I got too few votes, now I am Follower again')
             self._state = RaftServerState.Follower
-            await self._reset_election_timer()
         return
 
     async def _send_heartbeats(self):
+        # Upon election: Send initial empty AppendEntries RPCs (heartbeat) to each server
         if not self._state == RaftServerState.Leader:
             return
         
-        self.log('Send heartbeats')
+        self.echo('Send heartbeats')
         
         __tasks = []
         for node in self._others:
             __task = asyncio.create_task(self._append_entries(node, True))
             __tasks.append(__task)
+        
+        # Repeat during idle periods to prevent election timeouts
         await self._heartbeat_timer.start()
         
+        # Gather? Maybe, but now such solution:
         for __task in __tasks:
             await __task
     
@@ -267,7 +282,7 @@ class RaftServer:
         if not self._state == RaftServerState.Candidate:
             return
         
-        self.log('Try request votes...')
+        self.echo('Try request votes...')
 
         last_log_index = len(self._log)
         last_log_term = self._log.last if self._log else 0
@@ -292,7 +307,7 @@ class RaftServer:
                 timeout=self._election_timeout / 1000
             )
         except asyncio.TimeoutError as e:
-            self.log(f'Request votes: Timeout error')
+            self.echo(f'Request votes: Timeout error')
             return 0
         
         votes = 0
@@ -303,31 +318,108 @@ class RaftServer:
             try:
                 r = RaftRequestVoteResponse(**r)
             except pydantic.ValidationError as e:
-                self.log(f'Request Votes: Response parsing error: {e}')
+                self.echo(f'Request Votes: Response parsing error: {e}')
                 continue
             if r.voteGranted:
                 votes += 1
         return votes
     
     async def _become_follower(self, term: int):
-        self._log(f'Became Follower with term {term}')
+        self.echo(f'Became Follower with term {term}')
         self._current_term = term
         self._state = RaftServerState.Follower
         self._voted_for = None
         self._leader = None
+        await self._reset_election_timer()
     
     async def _become_leader(self):
-        pass
+        self._state = RaftServerState.Leader
+        self._leader = self._this
+        # for each server,
+        # index of the next log entry to send to that server
+        self._next_index = { node: len(self._log) + 1 for node in self._others }
+        # for each server, 
+        # index of highest log entry known to be replicated on server
+        self._match_index = { node: 0 for node in self._others }
 
     async def _append_entries(self, node: RaftNodeAddress, is_heartbeat: bool = False):
-        pass
+        # Invoked by leader to replicate log entries
+        # Also used as heartbeat
+        if not self._state == RaftServerState.Leader:
+            return
+        
+        self.echo(f'Try append entries for node {node}...')
+        
+        # When sending an AppendEntries RPC, the leader includes the index
+        # and term of the entry in its log that immediately precedes
+        # the new entries.
+        next_log_index = self._next_index.get(node, 0)
+        prev_log_index = next_log_index - 1
+        prev_log_term = self._log[prev_log_index] if self._log else 0
     
+        # log entries to store (may send more than one for efficiency)
+        if next_log_index < len(self._log):
+            entries = self._log[next_log_index:]
+        else:
+            entries = []
+        
+        # empty for heartbeat
+        if is_heartbeat:
+            entries = []
+
+        data = RaftAppendEntriesRequest(
+            term = self._current_term,
+            leaderId = str(self._this),
+            prevLogIndex = prev_log_index,
+            prevLogTerm = prev_log_term,
+            entries = entries,
+            leaderCommit = self._commit_index
+        ).model_dump()
+        
+        route = self._RPC_APPEND_ENTRIES_URL
+
+        __tasks = []
+        for node in self._others:
+            __task = asyncio.create_task()
+            __tasks.append(__task)
+
+        r = await self._send_rpc(node, route, data)
+        
+        if r is None:
+            self.echo(f'Append entries for {node}: Response is None')
+            return
+        
+        try:
+            r = RaftAppendEntriesResponse(**r)
+        except pydantic.ValidationError as e:
+            self.echo(f'Append Entries for {node}: Response parsing error:')
+            return
+        
+        if r.term > self._current_term:
+            await self._become_follower(r.term)
+            return
+        
+        if r.success:
+            # Update nextIndex and matchIndex for Follower
+            self._next_index[node] = prev_log_index + len(entries) - 1
+            self._match_index[node] = self._next_index[node] - 1
+            # Update commitIndex
+            # ...
+        else:
+            # After a rejection, the leader decrements nextIndex and retries
+            # the AppendEntries RPC.
+            self._next_index[node] = max(1, self._next_index[node] - 1)
+            # When nextIndex will reach a point where the leader and follower logs match,
+            # AppendEntries will succeed, which removes any conflicting entries 
+            # in the follower’s log and appends entries from the leader’s log (if any).
+        return
+
     async def _raft_rpc_request_vote(self, request: web.Request):
         try:
             data = await request.json()
             r = RaftRequestVoteRequest(**data)
         except (json.JSONDecodeError, pydantic.ValidationError):
-            self.log('Raft RCP: Request Vote: Request parsing error')
+            self.echo('Raft RCP: Request Vote: Request parsing error')
             msg = {'error': 'Invalid JSON'}
             return web.json_response(msg, status=400)
         
@@ -336,38 +428,44 @@ class RaftServer:
         lastLogIndex = r.lastLogIndex
         lastLogTerm = r.lastLogTerm
         
+        # If RPC request or response contains term T > currentTerm: 
+        # set currentTerm = T, convert to follower
+        if term > self._current_term:
+            await self._become_follower(term)
+        
+        #  Reply false if term < currentTerm
         if term < self._current_term:
             response = RaftRequestVoteResponse(
                 self._current_term, False
             ).model_dump()
             return web.json_response(response)
         
-        if term > self._current_term:
-            await self._become_follower(term)
-        
+        # if already voted for self or another candidate
         if self._voted_for is not None and self._voted_for != candidateId:
             response = RaftRequestVoteResponse(
                 self._current_term, False
             ).model_dump()
             return web.json_response(response)
         
+        # if candidate’s log is not up-to-date
         if lastLogIndex < len(self._log):
             response = RaftRequestVoteResponse(
                 self._current_term, False
             ).model_dump()
             return web.json_response(response)
         
-        if not self._log or lastLogTerm >= self._log.last.term:
-            self._voted_for = RaftNodeAddress(candidateId)
-            await self._reset_election_timer()
-
+        # if candidate’s log is not up-to-date
+        if self._log and lastLogTerm < self._log.last.term:
             response = RaftRequestVoteResponse(
-                self._current_term, True
+                self._current_term, False
             ).model_dump()
             return web.json_response(response)
         
+        self._voted_for = RaftNodeAddress(candidateId)
+        await self._reset_election_timer()
+
         response = RaftRequestVoteResponse(
-            self._current_term, False
+            self._current_term, True
         ).model_dump()
         return web.json_response(response)
     
