30e48008c44030a6e117c37ed2c443bca2a92f2e
g-vinokurov <g.vinokurov@g.nsu.ru>
2025-04-17 07:41:30 +0700

    Add Exceptions Handler

--- AppClient.py
+++ AppClient.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import QApplication
 
 from Gui.Widgets.Window import Window
 
+from Exceptions import Handler
+
+Handler.connect()
+
 
 class App(QApplication):
     def __init__(self, *args, **kwargs):
